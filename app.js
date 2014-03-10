e893b5651b68aeabb02297d1d11cf9a8585fb1dc
gilsondev <contato@gilsondev.com>
2014-03-10 15:15:33 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var express = require('express');
+var load = require('express-load');
+var routes = require('./routes');
+var app = express();
+
+
+app.set('views', __dirname + '/views');
+app.set('view, engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', routes.index);
+app.get('/usuarios', routes.user.index);
+
+load('models')
+    .then('controllers')
+    .then('routes')
+    .into(app);
+
+app.listen(3000, function() {
+    console.log("Ntalk no ar.");
+});
---
+++ controllers/home.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(app) {
+    var HomeController = {
+        index: function(req, res) {
+            res.render('home/index');
+        }
+    };
+    return HomeController;
+}
