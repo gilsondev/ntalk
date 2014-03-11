e51232ec985ea9f656dbe5a068b1a495a370cc5e
gilsondev <contato@gilsondev.com>
2014-03-11 09:46:31 -0300

    Insert login and logout action

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,28 @@
 'use strict';
 
 module.exports = function(app) {
+    var Usuario = app.models.usuario;
+
     var HomeController = {
         index: function(req, res) {
             res.render('home/index');
+        },
+        login: function(req, res) {
+            var email = req.body.usuario.email;
+            var nome = req.body.usuario.nome;
+
+            if(email && nome) {
+                var usuario = req.body.usuario;
+                usuario['contatos'] = []
+                req.session.usuario = usuario;
+                res.redirect('/contato');
+            } else {
+                res.redirect('/');
+            }
+        },
+        logout: function(req, res) {
+            req.session.destroy();
+            res.redirect('/');
         }
     };
     return HomeController;
