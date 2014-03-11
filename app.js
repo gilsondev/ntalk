f8f2d407bf934113a6f9ae7adff190bf1cb1729f
gilsondev <contato@gilsondev.com>
2014-03-11 09:46:54 -0300

    Insert cookies, session and json feature

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ var app = express();
 
 app.set('views', __dirname + '/views');
 app.set('view, engine', 'ejs');
+app.use(express.cookieParser('ntalk'));
+app.use(express.session());
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(express.static(__dirname + '/public'));
 
 
